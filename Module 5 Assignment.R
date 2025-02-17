7d8cd88c1c8dfcc62c23894d6ba52f0a529282b8
ANance02 <anthonynance@usf.edu>
2025-02-16 20:53:36 -0500

    Add files via upload

---
+++ Module 5 Assignment.R
@@ -0,0 +1,18 @@
+A <- matrix(1:100, nrow = 10)
+B <- matrix(1:1000, nrow = 10)
+
+# Determinant of Matrix A
+detA <- det(A)
+detA
+
+# Inverse of Matrix A
+inverseA <- solve(A)
+inverseA
+
+# Determinant of Matrix B
+detB <- det(B)
+
+# Inverse of Matrix B
+inverseB <- solve(B)
+inverseB
+
